093ded1102696f42e4b69ffbaa57afe65ff5a64a
coded by urmi <urmilarajapurkar953@gmail.com>
2026-08-20 07:02:24 +0530

    feat: configure Prisma PostgreSQL database connection

---
+++ src/config/prisma.ts
@@ -0,0 +1,15 @@
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/client/client";
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({
+    adapter,
+});
+
+export default prisma;
